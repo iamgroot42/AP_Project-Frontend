54ee835ab907677c6daf45462ea2503fdc556bb7
Anshuman <anshuman14021@iiitd.ac.in>
2015-09-11 07:43:45 +0530

    Made controller class(basic), added FXML tags for first tab

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,94 @@
 package application;
 
-public class Controller {
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+public class Controller
+{
+	@FXML private TextField email_x; 
+	@FXML private TextField name_x; 
+	@FXML private TextField mob_x; 
+	@FXML private TextField fname_x;
+	@FXML private TextField pin_x;
+	@FXML private TextArea addc_x;
+	@FXML private TextArea addp_x;
+	@FXML private RadioButton stream1_x;
+	@FXML private RadioButton stream2_x;
+	@FXML private RadioButton stream3_x;
+	@FXML private RadioButton genderm_x;
+	@FXML private RadioButton genderf_x;
+	@FXML private RadioButton phyy_x;
+	@FXML private RadioButton phyn_x;
+	@FXML private RadioButton wary_x;
+	@FXML private RadioButton warn_x;
+	@FXML private ChoiceBox<String> pref1_x;
+	@FXML private ChoiceBox<String> pref2_x;
+	@FXML private ChoiceBox<String> pref3_x;
+	@FXML private ChoiceBox<String> category_x;
+	@FXML private ChoiceBox<String> nation_x;
+	@FXML private DatePicker dob_x;
+	@FXML private TabPane t;
+	@FXML private Tab t1;
+	@FXML private Tab t2;
+	@FXML private Tab t3;
+	@FXML private Tab t4;
+	@FXML private Tab t5;
+	Main mane;
+	
+	public void setMainApp(Main p)
+	{
+		this.mane=p;
+	}
+	
+	@FXML
+	public void initialize_() {	
+        	t.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>()
+			{	
+        		@Override
+				public void changed(ObservableValue<? extends Tab> tab, Tab oldTab, Tab newTab) {
+					if(newTab==t1)
+					{
+//						L1.setText("");
+//						budget.setText("");
+					}
+					else if(newTab==t2)
+					{
+//						L2.setText("");
+//						L3.setText("");
+//						L4.setText("");
+//						L5.setText("");
+//						category.setText("");
+//						amount.setText("");
+//						date.setValue(null);
+//						L6.setText("Total Budget: Rs."+mane.getBUD());
+//						L7.setText("Available Budget: Rs."+mane.getAVL());
+					}
+					else if(newTab==t3)
+					{
+						//Table
+//						L6.setText("Total Budget: Rs."+mane.getBUD());
+//						L7.setText("Total Expense: Rs."+(mane.getBUD()-mane.getAVL()));
+					}
+					else
+					{	
+						//Pie Chart
+//						Charter();
+					}
+			}});
+//        	dump.setEditable(false);
+//        	col1.setCellValueFactory(x -> new SimpleStringProperty(x.getValue().getCategory()));
+//        	col2.setCellValueFactory(x -> new SimpleIntegerProperty(x.getValue().getAmount()).asObject());
+//        	col3.setCellValueFactory(x -> new SimpleStringProperty(x.getValue().getDt()));
+        	
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,55 @@
 package application;
 	
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
+public class Main extends Application
+{
 
-public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+	public void start(Stage p){
+		AnchorPane a=null;
+		try
+		{
+			FXMLLoader b=new FXMLLoader();
+			b.setLocation(Main.class.getResource("View.fxml"));
+			a=(AnchorPane)b.load();
+			p.setScene(new Scene(a));
+			p.setTitle("PhD Application Form");
+			p.show();
+//			Controller x=b.getController();
+//			x.setMainApp(this);
+//			x.initialize_();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -23,4 +57,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
