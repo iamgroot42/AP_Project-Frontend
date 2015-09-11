d8f15cbbdf23560a15c4c625bdaa86a2b14d7cbf
Anshuman <anshuman14021@iiitd.ac.in>
2015-09-11 10:06:10 +0530

    Replaced custom getters/setters with auto-generated setters/getters

--- src/application/Model.java
+++ src/application/Model.java
@@ -39,21 +39,55 @@ class Post_Graduate extends CheckEmpty
 		department="";degree="";title="";
 		marks=-1;
 	}
-	public int getYear() {return year;}
-	public void setCollege(String x) {college=x;}
-	public String getCollege() {return college;}
-	public void setMarks(int x) {marks=x;}
-	public int getMarks() {return marks;}
-	public void setCity(String x) {city=x;}
-	public String getCity() {return city;}
-	public void setState(String x){state=x;}
-	public String getState() {return state;}
-	public void setDegree(String x) {degree=x;}
-	public String getDegree() {return degree;}
-	public void setDepartment(String x) {department=x;}
-	public String getDepartment() {return department;}
-	public void setTitle(String x) {title=x;}
-	public String getTitle() {return title;}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
 }
 
 class ECE extends CheckEmpty
@@ -64,8 +98,12 @@ class ECE extends CheckEmpty
 		preferences=new String[4];
 		preferences[0]=a;preferences[1]=b;preferences[2]=c;preferences[3]="";
 	}
-	public void setOptional(String x) {preferences[3]=x;}
-	public String[] getPreferences() {return preferences;}
+	public String[] getPreferences() {
+		return preferences;
+	}
+	public void setPreferences(String[] preferences) {
+		this.preferences = preferences;
+	}
 }
 
 class Other_Degree extends CheckEmpty
@@ -90,15 +128,37 @@ class Gate extends CheckEmpty
 	int year,rank;
 	float marks,score;
 	public Gate(int x) {year=x;area="";rank=-1;marks=score=-1;}
-	public int getYear() {return year;}
-	public void setScore(float x) {score=x;}
-	public float getScore() {return score;}
-	public void setMarks(float x) {marks=x;}
-	public float getMarks() {return marks;}
-	public void setArea(String x) {area=x;}
-	public String getArea() {return area;}
-	public void setRank(int x) {rank=x;}	
-	public int getRank() {return rank;}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public float getMarks() {
+		return marks;
+	}
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	
 }
 
 class Personal
@@ -133,24 +193,103 @@ class Personal
 		add_permanent=p_add;
 		pincode="";
 	}
-	public String getName() {return name;}
-	public String getEmail() {return email;}
-	public String getAddCorrespondence() {return add_correspondence;}
-	public String getMobile() {return mobile;}
-	public int getStream() {return phd_stream;}
-	public int getGender() {return gender;}
-	public String getCategory() {return category;}
-	public int getPhysical(){return physically_disabled;}
-//	public Date getDOB() {return date_of_birth;}
-	public int getWar() {return war_category;}
-	public String getFatherName() {return father_name;}
-	public String getNationality() {return nationality;}
-	public String getAddPermanent() {return add_permanent;}
-	public String getPincode() {return pincode;}
-	public String getPref4() {return preference[3];}
-	public String[] getPreference() {return preference;}
-	public void setPref4(String x) {preference[3]=x;}
-	public void setPincode(String x){pincode=x;}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getAdd_correspondence() {
+		return add_correspondence;
+	}
+	public void setAdd_correspondence(String add_correspondence) {
+		this.add_correspondence = add_correspondence;
+	}
+	public String getAdd_permanent() {
+		return add_permanent;
+	}
+	public void setAdd_permanent(String add_permanent) {
+		this.add_permanent = add_permanent;
+	}
+	public String getFather_name() {
+		return father_name;
+	}
+	public void setFather_name(String father_name) {
+		this.father_name = father_name;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public int getPhd_stream() {
+		return phd_stream;
+	}
+	public void setPhd_stream(int phd_stream) {
+		this.phd_stream = phd_stream;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public int getPhysically_disabled() {
+		return physically_disabled;
+	}
+	public void setPhysically_disabled(int physically_disabled) {
+		this.physically_disabled = physically_disabled;
+	}
+	public int getWar_category() {
+		return war_category;
+	}
+	public void setWar_category(int war_category) {
+		this.war_category = war_category;
+	}
+	public String[] getPreference() {
+		return preference;
+	}
+	public void setPreference(String[] preference) {
+		this.preference = preference;
+	}
+	public String getReftype() {
+		return reftype;
+	}
+	public void setReftype(String reftype) {
+		this.reftype = reftype;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public DatePicker getDate_of_birth() {
+		return date_of_birth;
+	}
+	public void setDate_of_birth(DatePicker date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+	
 }
 
 class Education 
@@ -177,25 +316,138 @@ class Education
 		ece_phd=ece;post_graduate=pg;
 		other_degree=other;given_gate=sat;
 	}
-	public String getXBoard() {return x_board;}
-	public float getXMarks() {return x_marks;}
-	public int getXyear() {return x_year;}
-	public String getXIIBoard() {return xii_board;}
-	public float getXIIMarks() {return xii_marks;}
-	public int getXIIyear() {return xii_year;}
-	public String getDegree() {return degree;}
-	public String getDepartment() {return department;}
-	public String getCollege() {return college;}
-	public String getUniversity() {return university;}
-	public String getCity() {return city;}
-	public String getState() {return state;}
-	public int getGradYear() {return graduation_year;}
-	public float getGradMarks() {return graduation_marks;}
-	public int getECEStatus() {return ece_phd;}
-	public int getPostStatus() {return post_graduate;}
-	public int getOtherStatus() {return other_degree;}
-	public int getGATEStatus() {return given_gate;}
-	public String getAchievements() {return achievements;}
+	public int getX_year() {
+		return x_year;
+	}
+	public void setX_year(int x_year) {
+		this.x_year = x_year;
+	}
+	public int getXii_year() {
+		return xii_year;
+	}
+	public void setXii_year(int xii_year) {
+		this.xii_year = xii_year;
+	}
+	public int getGraduation_year() {
+		return graduation_year;
+	}
+	public void setGraduation_year(int graduation_year) {
+		this.graduation_year = graduation_year;
+	}
+	public int getEce_phd() {
+		return ece_phd;
+	}
+	public void setEce_phd(int ece_phd) {
+		this.ece_phd = ece_phd;
+	}
+	public int getPost_graduate() {
+		return post_graduate;
+	}
+	public void setPost_graduate(int post_graduate) {
+		this.post_graduate = post_graduate;
+	}
+	public int getOther_degree() {
+		return other_degree;
+	}
+	public void setOther_degree(int other_degree) {
+		this.other_degree = other_degree;
+	}
+	public int getGiven_gate() {
+		return given_gate;
+	}
+	public void setGiven_gate(int given_gate) {
+		this.given_gate = given_gate;
+	}
+	public float getX_marks() {
+		return x_marks;
+	}
+	public void setX_marks(float x_marks) {
+		this.x_marks = x_marks;
+	}
+	public float getXii_marks() {
+		return xii_marks;
+	}
+	public void setXii_marks(float xii_marks) {
+		this.xii_marks = xii_marks;
+	}
+	public float getGraduation_marks() {
+		return graduation_marks;
+	}
+	public void setGraduation_marks(float graduation_marks) {
+		this.graduation_marks = graduation_marks;
+	}
+	public String getX_board() {
+		return x_board;
+	}
+	public void setX_board(String x_board) {
+		this.x_board = x_board;
+	}
+	public String getXii_board() {
+		return xii_board;
+	}
+	public void setXii_board(String xii_board) {
+		this.xii_board = xii_board;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public String getUniversity() {
+		return university;
+	}
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getAchievements() {
+		return achievements;
+	}
+	public void setAchievements(String achievements) {
+		this.achievements = achievements;
+	}
+	public Other_Degree getOD() {
+		return OD;
+	}
+	public void setOD(Other_Degree oD) {
+		OD = oD;
+	}
+	public Gate getG() {
+		return G;
+	}
+	public void setG(Gate g) {
+		G = g;
+	}
+	public Post_Graduate getPG() {
+		return PG;
+	}
+	public void setPG(Post_Graduate pG) {
+		PG = pG;
+	}
 }
 
 class Payment
@@ -203,8 +455,30 @@ class Payment
 	boolean clear;
 	String bank,branch,draft;
 	public Payment(String a,String b,String c) {clear=false;bank=a;branch=b;draft=c;}
-	public void setStatus(boolean x) {clear=x;}
-	public boolean getStatus() {return clear;}
+	public boolean isClear() {
+		return clear;
+	}
+	public void setClear(boolean clear) {
+		this.clear = clear;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getDraft() {
+		return draft;
+	}
+	public void setDraft(String draft) {
+		this.draft = draft;
+	}
 }
 
 public class Model implements Serializable {
